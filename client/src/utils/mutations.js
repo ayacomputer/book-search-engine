39ce6366b118ae2a0a848866106c2d7c507813c0
Ayako Woollan <ayako.woollan@gmail.com>
2022-04-30 23:20:54 +1000

    updating queries & mutations

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,56 @@
+import { gql } from '@apollo/client';
+
+export const USER_LOGIN = gql`
+mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      profile {
+        _id
+        name
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+mutation addUser(username: String!, email: String!, password: String!){
+    addUser( username: $username, email: $email, password: $password ) {
+        token
+      user {
+        _id
+        username
+        email
+      }
+    }
+}`;
+
+
+export const SAVE_BOOK = gql`
+mutation saveBook( authors: [String], description:String!, title: String!, bookId: ID!, image:String, link:String) {
+   saveBook saveBook( authors: $authors, description: $description, title: $title, bookId: $bookId, image: $image, link: $link) {
+       token 
+       user {
+        _id
+        username
+        email
+        bookCount
+        savedBooks
+     
+       }
+   }
+}`;
+
+
+export const REMOVE_BOOK = gql`
+mutation removeBook(bookId: ID!) {
+    removeBook(bookId: $bookId) {
+        token
+        user {
+        _id
+        username
+        email
+        bookCount
+        savedBooks
+        }
+    }
+} `;
